f1ff33d70e34a90bdc38cd0c47cc09f36635ebfe
Ishaan Alidina EPCC FPGA Testbed <74453831+ishaanalidina@users.noreply.github.com>
2025-12-16 16:36:36 +0000

    Replacing the m_axi interfaces with axis interfaces.

--- HLS_examples/interface/interface_example.cpp
+++ HLS_examples/interface/interface_example.cpp
@@ -6,9 +6,9 @@ extern "C"
     {
         //pragmas go here!
         //assign A and B to separate bundles so they have independent interfaces
-#pragma HLS INTERFACE mode=m_axi port=A bundle=a_bundle
-#pragma HLS INTERFACE mode=m_axi port=B bundle=b_bundle
-#pragma HLS INTERFACE mode=m_axi port=out bundle=a_bundle
+#pragma HLS INTERFACE mode=axis port=A
+#pragma HLS INTERFACE mode=axis port=B
+#pragma HLS INTERFACE mode=axis port=out
 V_ADD_LOOP:
         for(int i = 0; i < N_VALS; i++)
         {
