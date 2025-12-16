1263335ecb3b5102258d868b33852dc9e4e9bb1d
Ishaan Alidina EPCC FPGA Testbed <74453831+ishaanalidina@users.noreply.github.com>
2025-12-16 16:36:26 +0000

    Updated the interface example to complete step 2, including pragmas for the different arrays.

--- HLS_examples/interface/interface_example.cpp
+++ HLS_examples/interface/interface_example.cpp
@@ -6,6 +6,9 @@ extern "C"
     {
         //pragmas go here!
         //assign A and B to separate bundles so they have independent interfaces
+#pragma HLS INTERFACE mode=m_axi port=A bundle=a_bundle
+#pragma HLS INTERFACE mode=m_axi port=B bundle=b_bundle
+#pragma HLS INTERFACE mode=m_axi port=out bundle=a_bundle
 V_ADD_LOOP:
         for(int i = 0; i < N_VALS; i++)
         {
